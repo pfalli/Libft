13d3da2a4523ccd6e380a3d438f9342a739a6957
Pierotti Falli <pfalli@student.42wolfsburg.de>
2023-11-29 17:12:02 +0100

    lll

--- ft_split.c
+++ ft_split.c
@@ -6,38 +6,44 @@
 /*   By: pfalli <pfalli@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 11:14:56 by pfalli            #+#    #+#             */
-/*   Updated: 2023/11/29 13:35:12 by pfalli           ###   ########.fr       */
+/*   Updated: 2023/11/29 17:06:53 by pfalli           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// the function split the array of words putting in the middle a (char c)
+// the function split the array of words putting in the middle (char c)
 
 #include "libft.h"
 
 char *nwords (char const *s, char c)
 {
-	int word = 0;
-	int i = 0;
-	int *j = 0;
-	char *arr;
-
-	while ( c && arr[i] != 0)
+	int count = 0;
+	int x = 0;
+	
+	while (*s)
 	{
-		while ( c && arr[i[j]] != 0)
+		if(*s != c && x ==0)
 		{
-			
+			count++;
+			x = 1;
 		}
+		else if (*s == c)
+			x = 0;
+		s++;
 	}
+	return (count);
 }
 
-void ft_free()
+void ft_free(void *ptr)
 {
-	
+	if(ptr != NULL)
+		free(ptr);
 }
 
-char *copy()
+char *copyword(char *str, )
 {
-	
+	char *word;
+
+	word = malloc
 }
 
 
