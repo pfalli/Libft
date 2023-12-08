0d019d51629c5d7689a37cb3c9697a5c1541a2eb
Pierotti Falli <pfalli@student.42wolfsburg.de>
2023-12-08 11:40:37 +0100

    commit

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: pfalli <pfalli@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 11:14:56 by pfalli            #+#    #+#             */
-/*   Updated: 2023/12/07 16:30:37 by pfalli           ###   ########.fr       */
+/*   Updated: 2023/12/08 11:11:06 by pfalli           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,48 +14,19 @@
 
 #include "libft.h"
 
-char *process(char const *s, char c, char **split)
+char	**printing_word(char **split, const char *start, char const *s, int i)
 {
-	int			i;
-	int			lenword;
-	int			count;
-	const char	*start;
-	int			j;
-	char		**split;
+	int	j;
 
-	i = 0;
-	start = s;
-	while (*s)
+	j = 0;
+	while (start < s)
 	{
-		if (*s != c)
-		{
-			start = s;
-			while (*s && *s != c)
-				s++;
-			lenword = s - start;
-			split[i] = (char *)malloc((lenword + 1) * sizeof(char));
-			if (split[i] == NULL)
-			{
-				ft_free(split, i);
-				return (NULL);
-			}
-			j = 0;
-			while (start < s)
-			{
-				split[i][j] = *start;
-				j++;
-				start++;
-			}
-			split[i][j] = 0;
-			i++;
-		}
-		else
-		{
-			s++;
-		}
-		return
+		split[i][j] = *start;
+		j++;
+		start++;
 	}
-	return ()
+	split[i][j] = 0;
+	return (split);
 }
 
 int	countwords(char const *s, char c)
@@ -96,18 +67,12 @@ static void	*ft_free(char **strs, int count)
 char	**ft_split(char const *s, char c)
 {
 	int			i;
-	int			lenword;
-	int			count;
-	const char	*start = s;
-	int			j;
+	const char	*start;
 	char		**split;
 
 	i = 0;
-	count = countwords(s, c);
-	if (s == NULL)
-		return (NULL);
-	split = (char **)malloc((count + 1) * sizeof(char *));
-	if (split == NULL)
+	split = (char **)malloc((countwords(s, c) + 1) * sizeof(char *));
+	if (split == NULL || s == NULL)
 		return (NULL);
 	while (*s)
 	{
@@ -116,48 +81,36 @@ char	**ft_split(char const *s, char c)
 			start = s;
 			while (*s && *s != c)
 				s++;
-			lenword = s - start;
-			split[i] = (char *)malloc((lenword + 1) * sizeof(char));
+			split[i] = (char *)malloc(((s - start) + 1) * sizeof(char));
 			if (split[i] == NULL)
-			{
 				ft_free(split, i);
-				return (NULL);
-			}
-			j = 0;
-			while (start < s)
-			{
-				split[i][j] = *start;
-				j++;
-				start++;
-			}
-			split[i][j] = 0;
+			printing_word(split, start, s, i);
 			i++;
 		}
 		else
 			s++;
 	}
-	split[i] = NULL;
-	return (split);
+	return (split[i] = NULL, split);
 }
 
-//	int	main(void)
-//	{
-//		int i = 0;
-//		char const *input = "Hello, this is a sample text";
-//		char **result = ft_split(input, ' ');
-//
-//		if (result)
-//		{
-//			while (result[i] != NULL)
-//			{
-//				printf("%s$$$", result[i]);
-//				i++;
-//			}
-//			free(result);
-//		}
-//		else
-//		{
-//			printf("Splitting failed.\n");
-//		}
-//		return (0);
-//	}
+// int	main(void)
+// {
+// 	int i = 0;
+// 	char const *input = "Hello, supp ahahah";
+// 	char **result = ft_split(input, ' ');
+
+// 	if (result)
+// 	{
+// 		while (result[i] != NULL)
+// 		{
+// 			printf("%s$$$", result[i]);
+// 			i++;
+// 		}
+// 		free(result);
+// 	}
+// 	else
+// 	{
+// 		printf("Splitting failed.\n");
+// 	}
+// 	return (0);
+// }
